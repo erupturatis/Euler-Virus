bc6f92b03beadec4f058e981c8621c8f6ee5e78b
erupturatis <bb.octav@gmail.com>
2022-02-06 21:47:21 +0200

    1

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
+'''
+
+How it runs : Image Encrypted
+Payload : When running download math documentation and euler photos and puts them on desktop
+Multiply Sistem : Via Discord
+'''
+def main():
+    pass
+
 
 
 if __name__ == '__main__':
-    pass
+    main()
     
