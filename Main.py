0e6cffbca2242b2ee58bcdd582841b2cd79b4122
erupturatis <bb.octav@gmail.com>
2022-02-09 19:07:17 +0200

    feat: initial structure

--- Main.py
+++ Main.py
@@ -1,11 +1,55 @@
-'''
+from pyclbr import Function
+from typing import List
+import requests
 
-How it runs : Image Encrypted
+'''
 Payload : When running download math documentation and euler photos and puts them on desktop
 Multiply Sistem : Via Discord
 '''
+
+
+
+class VirusCore():
+    def __init__(self,message: str):
+        self.message = message
+    
+    def custom_message(f):
+        def wrapper(self,*args,**kwargs):
+            print(*args)        # auxiliary message
+            f(self)
+            print(self.message) # virus message
+        return wrapper
+
+    @custom_message
+    def multiply_in_dms(self):
+        print("multiply_in_dms")
+
+    @custom_message
+    def multiply_in_severs(self):
+        pass
+    
+        
+
+    
+class AlgebraDownloader(VirusCore):
+    def __init__(self,message: str,request_list: List[str]):
+        super().__init__(message)
+        self.requests = request_list
+
+    def download(self):
+        pass
+
+class ChromeTabOpener(VirusCore):
+    def __init__(self,message: str,links_list: List[str]):
+        super().__init__(message)
+        self.links = links_list
+
+
 def main():
     pass
+    virus = VirusCore("testing virus")
+    virus.multiply_in_dms("mul1","mul2")
+
 
 
 
