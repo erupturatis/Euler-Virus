d690691a1a4dd1172964ae214b5e94adf5ec11a2
erupturatis <bb.octav@gmail.com>
2022-02-12 21:53:14 +0200

    feat:saving files to desktop

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from pyclbr import Function
+from re import A
 from typing import List
 import requests
+import os
 
 '''
 Payload : When running download math documentation and euler photos and puts them on desktop
@@ -12,30 +14,21 @@ Multiply Sistem : Via Discord
 class VirusCore():
     def __init__(self,message: str):
         self.message = message
-    
-    def custom_message(f):
-        def wrapper(self,*args,**kwargs):
-            print(*args)        # auxiliary message
-            f(self)
-            print(self.message) # virus message
-        return wrapper
 
-    @custom_message
     def multiply_in_dms(self):
         print("multiply_in_dms")
 
-    @custom_message
+
     def multiply_in_severs(self):
         pass
     
-        
 
     
 class AlgebraDownloader(VirusCore):
     def __init__(self,message: str,request_list: List[str]):
         super().__init__(message)
         self.requests = request_list
-
+        
     def download(self):
         pass
 
@@ -46,9 +39,24 @@ class ChromeTabOpener(VirusCore):
 
 
 def main():
-    pass
-    //virus = VirusCore("testing virus")
-    //virus.multiply_in_dms("mul1","mul2")
+    
+    ht = "http://httpbin.org/get"
+    a = "https://www.101books.ru/carte/descarca-adolf-hitler-mein-kampf-lupta-mea-pdf"
+    cluj = "https://admitereonline.utcluj.ro/docs/2021/matematica/Siruri_NReal_Baias_Popa.pdf"
+
+
+    r = requests.get(cluj)
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    path = os.path.join(desktop,'culegeretestare232.pdf')
+    with open(path , 'wb') as f:
+        f.write(r.content)
+
+
+    #req = list()
+    #req = ["http://www.upt.ro//img/files/2021-2022/Admitere/Licenta/Culegere_admitere_UPT_2022.pdf"]
+
+    #algebra_downloader = AlgebraDownloader("downloaded")
+
 
 
 
