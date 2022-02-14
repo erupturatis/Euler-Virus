b1ab7da07b49576525891b427ed738a3c9a5c979
erupturatis <bb.octav@gmail.com>
2022-02-14 21:10:33 +0200

    feat:class download implementation

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from re import A
 from typing import List
 import requests
 import os
-
+import webbrowser
 '''
 Payload : When running download math documentation and euler photos and puts them on desktop
 Multiply Sistem : Via Discord
@@ -12,9 +12,6 @@ Multiply Sistem : Via Discord
 
 
 class VirusCore():
-    def __init__(self,message: str):
-        self.message = message
-
     def multiply_in_dms(self):
         print("multiply_in_dms")
 
@@ -25,32 +22,26 @@ class VirusCore():
 
     
 class AlgebraDownloader(VirusCore):
-    def __init__(self,message: str,request_list: List[str]):
-        super().__init__(message)
+    def __init__(self,clone_number: int,message: str,request_list: List[str]):
         self.requests = request_list
+        self.clone_number = clone_number
         
     def download(self):
-        pass
+        for req in self.requests:
+            r = requests.get(req)
+            desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+            for i in range(self.clone_number):
+                path = os.path.join(desktop,f'culegeretest{i}.pdf')
+                with open(path , 'wb') as f:
+                    f.write(r.content)
+        return self
 
-class ChromeTabOpener(VirusCore):
-    def __init__(self,message: str,links_list: List[str]):
-        super().__init__(message)
-        self.links = links_list
+
 
 
 def main():
-    
-    ht = "http://httpbin.org/get"
-    a = "https://www.101books.ru/carte/descarca-adolf-hitler-mein-kampf-lupta-mea-pdf"
-    cluj = "https://admitereonline.utcluj.ro/docs/2021/matematica/Siruri_NReal_Baias_Popa.pdf"
-
-
-    r = requests.get(cluj)
-    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    path = os.path.join(desktop,'culegeretestare232.pdf')
-    with open(path , 'wb') as f:
-        f.write(r.content)
-
+    algebra_downloader = AlgebraDownloader(5,"",["http://www.upt.ro//img/files/2021-2022/Admitere/Licenta/Culegere_admitere_UPT_2022.pdf"])
+    algebra_downloader.download()
 
     #req = list()
     #req = ["http://www.upt.ro//img/files/2021-2022/Admitere/Licenta/Culegere_admitere_UPT_2022.pdf"]
